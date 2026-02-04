53a646ef930d6283d2bac22391cb0c49401ef0a6
AlfiyaSiddique <alfiyasiddique1708@gmail.com>
2026-02-04 14:23:13 +0530

    firebase fix

--- web/src/lib/firebase.js
+++ web/src/lib/firebase.js
@@ -4,9 +4,6 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
